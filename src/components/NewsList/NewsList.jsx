ed2d7f9136628a7a0399aac451c2aa5cdd2681c8
Vladimir <vovagrigoryan2003@gmail.com>
2024-12-29 21:24:43 +0300

    last

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,10 +1,11 @@
 import styles from "./styles.module.css"
+import NewsItem from "../NewsItem/NewsItem";
 
 const NewsList = ({news}) => {
     return (
         <ul className={styles.list}>
             {news.map(item => {
-                return <li key={item.id}>{item.title}</li>
+                return <NewsItem key={item.id} item={item} />
             })}
         </ul>
     );
